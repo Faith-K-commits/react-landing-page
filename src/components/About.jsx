7a2912bc45dd6f50bc888a2f64513a6ed7195782
Faith Kariuki <tcwamboi@gmail.com>
2024-09-03 15:02:04 +0300

    Fix image and about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 const About = () => {
     return (
-        // FIXME: Fix h1 and quote
         <div>
-            <div className='flex mt-8 gap-16'>
+            <div className='flex mt-8 space-x-16'>
                 <h1 className='text-gray-500 w-fit'>About Us</h1>
-                <p className='text-3xl'>"We believe quality furniture is key to a beautiful, functional home. <br /> With a passion for design and craftsmanship, we blend modern aesthetics with timeless elegance."</p>
+                <q className='text-2xl'>We believe quality furniture is key to a beautiful, functional home. <br /> With a passion for design and craftsmanship, we blend modern aesthetics with timeless elegance.</q>
             </div>
             <hr className='mt-2 text-gray-900 font-extrabold' />
         </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,14 +4,13 @@ import { ArrowDownCircleOutline } from 'react-ionicons'
 const HeroSection = () => {
     return (
         <div className='mt-4 flex justify-center relative'>
-            <div className='absolute ml-32 mt-6 mr-96'>
+            <div className='absolute mr-96 mt-6'>
                 <h1 className='font-bold text-6xl mb-4'>Discover Comfort,Style,and <br /> Quality Craftsmanship </h1>
                 <p>Our furniture embodies perfect blend of functionality and aesthetic appeal, ensuring every piece <br /> enhances your home with enduring elegance and superior durability.</p>
                 <button className='p-2 bg-black text-white rounded-lg cursor-pointer mt-4'>Join Membership</button>
             </div>
-            <span className='absolute mt-96 h-96 flex items-center'><ArrowDownCircleOutline /></span>
-            {/* FIXME:Reduce image size */}
-            <img src="./images/furniture.jpg" alt="Furniture set" className='rounded-lg' />
+            <span className='absolute mt-80 h-96 flex items-center justify-center cursor-pointer'><ArrowDownCircleOutline /></span>
+            <img src="./images/furniture.jpg" alt="Furniture set" className='rounded-lg w-full' />
         </div>
     )
 }
