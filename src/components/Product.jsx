8475bc58a65f6f962afab8b4906e0f107aee94c3
Faith Kariuki <tcwamboi@gmail.com>
2024-09-05 14:56:51 +0300

    feat: Update Product component to display product details

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 
 const Product = ({ products }) => {
     return (
-        <div>
-            {products.map(product => (
-                <div key={product.id}>
-                    <img src={product.image} alt="product image" />
-                    <p>{product.description}</p>
-                </div>
-            ))}
+        <div className='mt-8'>
+            <h1 className='font-bold text-3xl'>Interiors by Prabott.</h1>
+            <div className='grid grid-cols-4 gap-4 border-black rounded-lg'>
+                {products.map(product => (
+                    <div key={product.id} >
+                        <h1>{product.title}</h1>
+                        <img src={product.image} alt="product image" className='h-72 w-64' />
+                        <div className='flex justify-between'>
+                            <p className='text-gray-300'>{product.category}</p>
+                            <p>€{product.price}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from './Product'
 
 const Products = () => {
     const [products, setProducts] = useState([])
 
-    fetch("https://fakestoreapi.com/products")
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            setProducts(data)
-        })
-        .catch(error => console.error(error))
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data)
+            })
+            .catch(error => console.error(error))
+    }, [])
 
     return (
         <div>
