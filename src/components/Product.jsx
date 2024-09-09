0f993108aff3f64dfb3a0481a79330260c09409a
Faith Kariuki <tcwamboi@gmail.com>
2024-09-09 14:45:56 +0300

    Update Product component to display product details and improve styling

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,15 +3,14 @@ import React from 'react'
 const Product = ({ products }) => {
     return (
         <div className='mt-8'>
-            <h1 className='font-bold text-3xl'>Interiors by Prabott.</h1>
-            <div className='grid grid-cols-4 gap-4 border-black rounded-lg'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
                 {products.map(product => (
-                    <div key={product.id} >
-                        <h1>{product.title}</h1>
-                        <img src={product.image} alt="product image" className='h-72 w-64' />
-                        <div className='flex justify-between'>
-                            <p className='text-gray-300'>{product.category}</p>
-                            <p>€{product.price}</p>
+                    <div key={product.id} className='border border-gray-300 rounded-lg p-4'>
+                        <h2 className='font-semibold text-lg mb-2'>{product.title}</h2>
+                        <img src={product.image} alt="product image" className='h-72 w-full object-cover rounded-md mb-4' />
+                        <div className='flex justify-between items-center'>
+                            <p className='text-gray-500'>{product.category}</p>
+                            <p className='font-bold'>€{product.price}</p>
                         </div>
                     </div>
                 ))}
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Product from './Product'
+import { ArrowBackOutline, ArrowForwardOutline } from 'react-ionicons'
 
 const Products = () => {
     const [products, setProducts] = useState([])
 
     useEffect(() => {
-        fetch("https://fakestoreapi.com/products")
+        fetch("https://fakestoreapi.com/products?limit=8")
             .then(res => res.json())
             .then(data => {
                 setProducts(data)
@@ -15,6 +16,13 @@ const Products = () => {
 
     return (
         <div>
+            <div className='flex justify-between mt-8'>
+                <h1 className='font-bold text-5xl'>Interiors by Prabott.</h1>
+                {/* TODO: Make arrows dynamic */}
+                <div className='flex gap-4 cursor-pointer text-gray-500'>
+                    <ArrowBackOutline /> <ArrowForwardOutline />
+                </div>
+            </div>
             <Product products={products} />
         </div>
     )
